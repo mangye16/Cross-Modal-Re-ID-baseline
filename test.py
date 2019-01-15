30825a59b9bd4073a13a0de05f19d3f476366950
mangye16 <303463418@qq.com>
2019-01-15 12:42:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -15,7 +15,6 @@ from data_manager import *
 from eval_metrics import eval_sysu, eval_regdb
 from model import embed_net
 from utils import *
-
 import time 
 import scipy.io as scio
 
@@ -51,12 +50,17 @@ parser.add_argument('--mode', default='all', type=str, help='all or indoor')
 
 args = parser.parse_args() 
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
 np.random.seed(1)
 dataset = args.dataset
 if dataset == 'sysu':
     data_path = '/home/datasets/prml/computervision/re-id/sysu-mm01/ori_data'
     n_class = 395
-
+    test_mode = [1, 2] 
+elif dataset =='regdb':
+    data_path = 'RegDB/'
+    n_class = 206
+    test_mode = [2, 1]
  
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
@@ -71,7 +75,6 @@ print('==> Resuming from checkpoint..')
 checkpoint_path = args.model_path
 if len(args.resume)>0:   
     model_path = checkpoint_path + args.resume
-    # model_path = checkpoint_path + 'sysu_id_bn_relu_drop_0.5_lr_1.0e-02_dim_512_resnet50_best.t'
     if os.path.isfile(model_path):
         print('==> loading checkpoint {}'.format(args.resume))
         checkpoint = torch.load(model_path)
@@ -92,7 +95,6 @@ print('==> Loading data..')
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
 transform_train = transforms.Compose([
     transforms.ToPILImage(),
-    # transforms.Resize((280,150), interpolation=2),
     transforms.RandomCrop((args.img_h,args.img_w)),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
@@ -112,24 +114,37 @@ if dataset =='sysu':
     # testing set
     query_img, query_label, query_cam = process_query_sysu(data_path, mode = args.mode)
     gall_img, gall_label, gall_cam = process_gallery_sysu(data_path, mode = args.mode, trial = 0)
+
+      
+elif dataset =='regdb':
+    # training set
+    trainset = RegDBData(data_path, args.trial, transform=transform_train)
+    # generate the idx of each person identity
+    color_pos, thermal_pos = GenIdx(trainset.train_color_label, trainset.train_thermal_label)
     
-    nquery = len(query_label)
-    ngall = len(gall_label)
-    print("Dataset statistics:")
-    print("  ------------------------------")
-    print("  subset   | # ids | # images")
-    print("  ------------------------------")
-    print("  query    | {:5d} | {:8d}".format(len(np.unique(query_label)), nquery))
-    print("  gallery  | {:5d} | {:8d}".format(len(np.unique(gall_label)), ngall))
-    print("  ------------------------------")
+    # testing set
+    query_img, query_label = process_test_regdb(data_path, trial = args.trial, modal = 'visible')
+    gall_img, gall_label  = process_test_regdb(data_path, trial = args.trial, modal = 'thermal')
+    
+    gallset  = TestData(gall_img, gall_label, transform = transform_test, img_size =(args.img_w,args.img_h))
+    gall_loader  = data.DataLoader(gallset, batch_size=args.test_batch, shuffle=False, num_workers=args.workers)
+    
+nquery = len(query_label)
+ngall = len(gall_label)
+print("Dataset statistics:")
+print("  ------------------------------")
+print("  subset   | # ids | # images")
+print("  ------------------------------")
+print("  query    | {:5d} | {:8d}".format(len(np.unique(query_label)), nquery))
+print("  gallery  | {:5d} | {:8d}".format(len(np.unique(gall_label)), ngall))
+print("  ------------------------------")
 
-    test_mode = [1, 2]
-    queryset = TestData(query_img, query_label, transform = transform_test, img_size =(args.img_w, args.img_h))
-
+queryset = TestData(query_img, query_label, transform = transform_test, img_size =(args.img_w, args.img_h))   
 query_loader = data.DataLoader(queryset, batch_size=args.test_batch, shuffle=False, num_workers=4)
 print('Data Loading Time:\t {:.3f}'.format(time.time()-end))
 
 feature_dim = args.low_dim
+
 if args.arch =='resnet50':
     pool_dim = 2048
 elif args.arch =='resnet18':
@@ -176,49 +191,67 @@ query_feat, query_feat_pool = extract_query_feat(query_loader)
 all_cmc = 0
 all_mAP = 0 
 all_cmc_pool = 0
-  
-for trial in range(10):
-    gall_img, gall_label, gall_cam = process_gallery_sysu(data_path, mode = args.mode, trial = trial)
-    
-    trial_gallset = TestData(gall_img, gall_label, transform = transform_test,img_size =(args.img_w,args.img_h))
-    trial_gall_loader  = data.DataLoader(trial_gallset, batch_size=args.test_batch, shuffle=False, num_workers=4)
-    
-    gall_feat, gall_feat_pool = extract_gall_feat(trial_gall_loader)
-    
+if dataset =='regdb':
+    gall_feat, gall_feat_pool = extract_gall_feat(gall_loader)
     # fc feature 
     distmat = np.matmul(query_feat, np.transpose(gall_feat))
-    cmc, mAP  = eval_sysu(-distmat, query_label, gall_label,query_cam, gall_cam)
+    cmc, mAP  = eval_regdb(-distmat, query_label, gall_label)
     
     # pool5 feature
     distmat_pool = np.matmul(query_feat_pool, np.transpose(gall_feat_pool))
-    cmc_pool, mAP_pool = eval_sysu(-distmat_pool, query_label, gall_label,query_cam, gall_cam)
-    if trial ==0:
-        all_cmc = cmc
-        all_mAP = mAP
-        all_cmc_pool = cmc_pool
-        all_mAP_pool = mAP_pool
-    else:
-        all_cmc = all_cmc + cmc
-        all_mAP = all_mAP + mAP
-        all_cmc_pool = all_cmc_pool + cmc_pool
-        all_mAP_pool = all_mAP_pool + mAP_pool
-    # pdb.set_trace()
-    print ('Test Trial: {}'.format(trial))
+    cmc_pool, mAP_pool = eval_regdb(-distmat_pool, query_label, gall_label)
+
+    print ('Test Trial: {}'.format(args.trial))
     print('FC: top-1: {:.2%} | top-5: {:.2%} | top-10: {:.2%}| top-20: {:.2%}'.format(
         cmc[0], cmc[4], cmc[9], cmc[19]))
     print('mAP: {:.2%}'.format(mAP))
     print('POOL5: top-1: {:.2%} | top-5: {:.2%} | top-10: {:.2%}| top-20: {:.2%}'.format(
         cmc_pool[0], cmc_pool[4], cmc_pool[9], cmc_pool[19]))
     print('mAP: {:.2%}'.format(mAP_pool))
+    
+elif dataset =='sysu':
+    for trial in range(10):
+        gall_img, gall_label, gall_cam = process_gallery_sysu(data_path, mode = args.mode, trial = trial)
+        
+        trial_gallset = TestData(gall_img, gall_label, transform = transform_test,img_size =(args.img_w,args.img_h))
+        trial_gall_loader  = data.DataLoader(trial_gallset, batch_size=args.test_batch, shuffle=False, num_workers=4)
+        
+        gall_feat, gall_feat_pool = extract_gall_feat(trial_gall_loader)
+        
+        # fc feature 
+        distmat = np.matmul(query_feat, np.transpose(gall_feat))
+        cmc, mAP  = eval_sysu(-distmat, query_label, gall_label,query_cam, gall_cam)
+        
+        # pool5 feature
+        distmat_pool = np.matmul(query_feat_pool, np.transpose(gall_feat_pool))
+        cmc_pool, mAP_pool = eval_sysu(-distmat_pool, query_label, gall_label,query_cam, gall_cam)
+        if trial ==0:
+            all_cmc = cmc
+            all_mAP = mAP
+            all_cmc_pool = cmc_pool
+            all_mAP_pool = mAP_pool
+        else:
+            all_cmc = all_cmc + cmc
+            all_mAP = all_mAP + mAP
+            all_cmc_pool = all_cmc_pool + cmc_pool
+            all_mAP_pool = all_mAP_pool + mAP_pool
+        
+        print ('Test Trial: {}'.format(trial))
+        print('FC: top-1: {:.2%} | top-5: {:.2%} | top-10: {:.2%}| top-20: {:.2%}'.format(
+            cmc[0], cmc[4], cmc[9], cmc[19]))
+        print('mAP: {:.2%}'.format(mAP))
+        print('POOL5: top-1: {:.2%} | top-5: {:.2%} | top-10: {:.2%}| top-20: {:.2%}'.format(
+            cmc_pool[0], cmc_pool[4], cmc_pool[9], cmc_pool[19]))
+        print('mAP: {:.2%}'.format(mAP_pool))
 
-cmc = all_cmc /10 
-mAP = all_mAP /10
+    cmc = all_cmc /10 
+    mAP = all_mAP /10
 
-cmc_pool = all_cmc_pool /10 
-mAP_pool = all_mAP_pool /10
-print ('All Average:')
-print('FC: top-1: {:.2%} | top-5: {:.2%} | top-10: {:.2%}| top-20: {:.2%}'.format(cmc[0], cmc[4], cmc[9], cmc[19]))
-print('mAP: {:.2%}'.format(mAP))
-print('POOL5: top-1: {:.2%} | top-5: {:.2%} | top-10: {:.2%}| top-20: {:.2%}'.format(
-    cmc_pool[0], cmc_pool[4], cmc_pool[9], cmc_pool[19]))
-print('mAP: {:.2%}'.format(mAP_pool))
+    cmc_pool = all_cmc_pool /10 
+    mAP_pool = all_mAP_pool /10
+    print ('All Average:')
+    print('FC: top-1: {:.2%} | top-5: {:.2%} | top-10: {:.2%}| top-20: {:.2%}'.format(cmc[0], cmc[4], cmc[9], cmc[19]))
+    print('mAP: {:.2%}'.format(mAP))
+    print('POOL5: top-1: {:.2%} | top-5: {:.2%} | top-10: {:.2%}| top-20: {:.2%}'.format(
+        cmc_pool[0], cmc_pool[4], cmc_pool[9], cmc_pool[19]))
+    print('mAP: {:.2%}'.format(mAP_pool))
